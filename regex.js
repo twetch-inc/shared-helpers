c430ac344cce336b146d7920c7011fd7f1ba64d1
Harrison Beckerich <harrison@beckerich.org>
2020-03-11 01:58:06 -0400

    master update regex

--- regex.js
+++ regex.js
@@ -1,19 +1,23 @@
-const TWITTER_REGEX = /http(s)?:\/\/(.*\.)?twitter\.com\/[A-z0-9_/?=]+/;
-const SOUNDCLOUD_REGEX = /http(s)?:\/\/(.*\.)?soundcloud\.com\/[A-z0-9_/?=-]+/;
+const BITCOIN_FILES_REGEX = /http(s)?:\/\/(.*\.)?media\.bitcoinfiles\.org\/([A-z0-9]+)/;
 const MENTION_REGEX = /(@[0-9]+)/g;
+const POST_NEWLINE_REGEX = /\n{3,}/g;
+const SOUNDCLOUD_REGEX = /http(s)?:\/\/(.*\.)?soundcloud\.com\/[A-z0-9_/?=-]+/;
 const TWETCH_POST_REGEX = /http(s)?:\/\/(.*\.)?twetch\.app\/t\/([A-z0-9_/?=]+)/;
 const TWETCH_REPLY_REGEX = /http(s)?:\/\/(.*\.)?twetch\.app\/t\/[A-z0-9_/?=]+#([A-z0-9_/?=]+)/;
+const TWITTER_REGEX = /http(s)?:\/\/(.*\.)?twitter\.com\/[A-z0-9_/?=]+/;
+const VIZ_REGEX = /http(s)?:\/\/(.*\.)?viz\.cash\/v\/([A-z0-9]+)/;
 /* eslint-disable */
 const YOUTUBE_REGEX = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
 /* eslint-enable */
-const POST_NEWLINE_REGEX = /\n{3,}/g;
 
 module.exports = {
+	BITCOIN_FILES_REGEX,
+	MENTION_REGEX,
+	POST_NEWLINE_REGEX,
+	SOUNDCLOUD_REGEX,
 	TWETCH_POST_REGEX,
 	TWETCH_REPLY_REGEX,
 	TWITTER_REGEX,
-	MENTION_REGEX,
-	YOUTUBE_REGEX,
-	SOUNDCLOUD_REGEX,
-	POST_NEWLINE_REGEX
+	VIZ_REGEX,
+	YOUTUBE_REGEX
 };
