0c989f953f577b967ad0e6aed442f42b9521a832
Harrison Beckerich <harrison@beckerich.org>
2020-10-19 11:40:43 -0700

    disable regex lookahead in tim apples browser

--- regex.js
+++ regex.js
@@ -1,6 +1,7 @@
 const BITCOIN_FILES_REGEX = /http(s)?:\/\/(.*\.)?media\.bitcoinfiles\.org\/([A-z0-9]+)/;
 const BITCOIN_FILES_PREVIEW_REGEX = /http(s)?:\/\/(.*\.)?bitcoinfiles\.org\/t\/([A-z0-9_\/?=]+)/;
-const HASHTAG_CONTEXT_REGEX = /(?<=\s|^)([#\$][0-9a-zA-Z(_\.\,)]{1,})(?=\s|$)/gi;
+const HASHTAG_CONTEXT_REGEX = /([#\$][0-9a-zA-Z(_\.\,)]{1,})/gi;
+//const HASHTAG_CONTEXT_REGEX = /(?<=\s|^)([#\$][0-9a-zA-Z(_\.\,)]{1,})(?=\s|$)/gi;
 const HASHTAG_REGEX = /([#\$][0-9a-zA-Z(_\.\,)]{1,})/gi;
 const MENTION_REGEX = /(@[0-9]+)/g;
 const MULTI_PAY_REGEX = /\/(pay)(( @([0-9]+))+) \$([0-9]+(.[0-9]+)?)/;
